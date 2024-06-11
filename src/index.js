e3c9c3b763d1eb58f64add9db4bc4eff63f8089c
donatselmanii <124844058+donatselmanii@users.noreply.github.com>
2024-06-11 22:42:08 +0200

    Dizajnimi i Navbarit dhe Footerit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-// index.js
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import App from "./App";
+import { StateProvider } from "./context/StateProvider";
+
+const rootElement = document.getElementById("root");
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StateProvider>
+      <App />
+    </StateProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  rootElement
 );
