2f2ab298ee07c4d3d69c4dd1b3fb15eaa91976a8
donatselmanii <124844058+donatselmanii@users.noreply.github.com>
2024-06-11 22:44:17 +0200

    Dizajnimi i faes kryesore dhe shtimi i te dhenave per produktet kryesore.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import Ball from './components/Ball';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/AboutUs" element={<AboutUs />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
